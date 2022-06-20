f3c0aca3cf8c44cd7fc8942f2d00c722e5c762df
sidney mulwa <mulwasidney@outlook.com>
2022-06-20 17:54:08 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun('bathe my dog');
+//Function Expression
+const mondayWork = function(activity = 'go to the office'){
+    return (`This Monday, I will ${activity}.`);
+};
+mondayWork("work from home");
+
+//Scope Chains
+let wrapAdjective = function (visualFlair = "*"){
+    return function (adjective = "special"){
+        return(`You are ${visualFlair}${adjective}${visualFlair}!`);
+    }
+}
+
+wrapAdjective("%")("a dedicated programmer");
